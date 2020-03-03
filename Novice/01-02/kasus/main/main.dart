afcf61c251bb3f06aff1031b1a02c74921db771b
Mirzax <61675668+Mirzax@users.noreply.github.com>
2020-03-04 05:54:54 +0700

    commit
    day 2

--- Novice/01-02/kasus/main/main.dart
+++ Novice/01-02/kasus/main/main.dart
@@ -6,11 +6,25 @@ main() {
   for (int i = 0; i < n.length; i++ ) {
     int j = i-1;
     while(j > 0 && n[j-1]  > key) {
+    int i;
+    int j = i - 1;
+    int x = n[i];
+    int k = i + 2;
+    //INSERTION SORTING
+  for ( i = 0; i < n.length; i++ ) {
+        while(j > 0 && n[j - 1]  > x) {
       n[j]=
     n[j] = n[j-1];
+    n[j] = n[j - 1];
     j = j - 1;
   }
   n[j] = key;
+  n[j] = x;
   }
   print(n);
+
+  //BACKWARD SORTING
+
+    for ( i = n.length - 1; i >= 0; i--) {
+      while (k < n.length && x > n[k]);
 }
